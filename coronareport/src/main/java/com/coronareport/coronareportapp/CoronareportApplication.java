55d3d3ed8b521f5ab3256e824d4db6cd89af5489
12Ramji34@& <pallavidubey0823@gmail.com>
2022-09-17 22:32:39 +0530

    First Commit

---
+++ coronareport/src/main/java/com/coronareport/coronareportapp/CoronareportApplication.java
@@ -0,0 +1,13 @@
+package com.coronareport.coronareportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronareportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronareportApplication.class, args);
+	}
+
+}
---
+++ coronareport/src/test/java/com/coronareport/coronareportapp/CoronareportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coronareport.coronareportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronareportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
